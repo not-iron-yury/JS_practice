c1c0c3c694e1321ed29991632025eaea14d9d2a4
not-iron-yury <not.iron.yuriy@inbox.ru>
2025-04-09 10:35:37 +0300

    Базовый класс v.2

--- OOP/1. Создание базового класса/script.js
+++ OOP/1. Создание базового класса/script.js
@@ -1,20 +1,35 @@
+// Задача 1: Создание базового класса
 // Создай класс Product, у которого:
 // 1. есть свойства name и price, задаваемые в constructor;
 // 2. есть метод getInfo(), который возвращает строку: "Товар: <name>, цена: <price>₽"
-
 // Пример:
 // const apple = new Product('Яблоко', 25);
 // console.log(apple.getInfo()); // Товар: Яблоко, цена: 25₽
 
+// Задача 2: Расширим Product
+// Добавим:
+// Свойство discount (в процентах, например, 20 значит 20%).
+// Метод getFinalPrice() — возвращает цену с учётом скидки.
+// Переопредели getInfo(), чтобы она возвращала: "Товар: Яблоко, цена без скидки: 25₽, цена со скидкой: 20₽"
+// Пример:
+// const apple = new Product('Яблоко', 25, 20);
+// console.log(apple.getFinalPrice()); // 20
+// console.log(apple.getInfo()); // Товар: Яблоко, цена без скидки: 25₽, цена со скидкой: 20₽
+
 class Product {
-  constructor(name, price) {
+  constructor(name, price, discount) {
     this.name = name;
     this.price = price;
+    this.discount = discount;
   }
   getInfo() {
-    return `Товар: ${this.name}, цена: ${this.price}₽`;
+    return `Товар: ${this.name}, цена без скидки: ${this.price}₽, цена со скидкой: ${this.getFinalPrice()}₽`;
+  }
+  getFinalPrice() {
+    return this.price * (1 - this.discount / 100);
   }
 }
 
-const apple = new Product('Яблоко', 25);
-console.log(apple.getInfo()); // Товар: Яблоко, цена: 25₽
+const apple = new Product('Яблоко', 25, 20);
+console.log(apple.getFinalPrice()); // 20
+console.log(apple.getInfo());
